9e37eae5b17765feea13748e8956629037a543c0
rusz01 <ruszbaidhya@gmail.com>
2025-06-19 21:06:13 +0100

    CSS and adding useState to change textColor

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,28 @@ import Buttons from './components/buttons.jsx'
 function App() {
   const [color, setColor] = useState("orange")
 
+  const [nameM, setName] = useState("Grey")
+
+  const [textColor, setTextColor] = useState("white")
+
+
+
 
   return (
     <>
     <div className='w-full h-screen'
       style={{ backgroundColor: color }}>
       <div className='flex justify-center'>
-        <h1 className='text-5xl font-bold text-white'>Orange</h1>
+        <h1 className='text-5xl font-bold absolute top-100' style={{color: textColor}}>{nameM}</h1>
       </div>
-          <div className='p-5 bg-gray-200 rounded-2xl w-350 flex items-center justify-center mb-20'>
-             <Buttons name="Grey" color={setColor}/>
-             <Buttons name="Blue" color={setColor}/>
-             <Buttons name="Orange" color={setColor}/>
-             <Buttons name="Pink" color={setColor}/>
-             <Buttons name="Purple" color={setColor}/>
-             <Buttons name="Red" color={setColor}/>
-             <Buttons name="Yellow" color={setColor}/>
+          <div className='p-5 bg-gray-200 rounded-2xl w-350 flex items-center justify-center absolute inset-x-30 bottom-0 mb-20'>
+             <Buttons name="Grey" color={setColor} nameMm={setName} txtColor={setTextColor}/>
+             <Buttons name="Blue" color={setColor} nameMm={setName} txtColor={setTextColor}/>
+             <Buttons name="Orange" color={setColor} nameMm={setName} txtColor={setTextColor}/>
+             <Buttons name="Pink" color={setColor} nameMm={setName} txtColor={setTextColor}/>
+             <Buttons name="Purple" color={setColor} nameMm={setName} txtColor={setTextColor}/>
+             <Buttons name="Red" color={setColor} nameMm={setName} txtColor={setTextColor}/>
+             <Buttons name="Yellow" color={setColor} nameMm={setName} txtColor={setTextColor}/>
           </div>
     </div>
     </>
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,30 +1,44 @@
 import React from 'react'
 
-function buttons({name, color}) {
+function buttons({name, color, nameMm, txtColor}) {
 
 
 function changeColor() {
 
    if (name === "Grey") {  
     color("grey")
+    nameMm("Grey")
+      txtColor("white")
     } else if (name === "Blue") {
       color("blue")
+      nameMm("Blue")
+      txtColor("white")
     } else if (name === "Orange") {
       color("orange")
+      nameMm("Orange")
+      txtColor("white")
     } else if (name === "Pink") {
       color("pink")
+      nameMm("Pink")
+      txtColor("black")
     } else if (name === "Purple") {
       color("purple")
+      nameMm("Purple")
+      txtColor("white")
     } else if (name === "Red") {
       color("red")
+      nameMm("Red")
+      txtColor("white")
     } else if (name === "Yellow") {
       color("yellow")
+      nameMm("Yellow")
+      txtColor("black")
    }
 }
   return (
 
-      <button onClick={changeColor} className="px-8 py-3 mr-5 font-semibold rounded dark:bg-gray-100 dark:text-gray-800"
-      style={{ backgroundColor: name.toLowerCase() }}
+      <button onClick={changeColor} className="px-8 py-3 mr-5  rounded-xl"
+      style={{ backgroundColor: name.toLowerCase()}}
       >{name}</button>
      
   )
